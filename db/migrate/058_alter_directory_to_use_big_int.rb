995dfc87d6415809947a6b3a74910d85af0d83e9
Ben De Luca <you@example.com>
2011-02-16 17:16:51 +0000

    changed type of director count to be larger to accomadate filesystems with huge amounts of files

---
+++ db/migrate/058_alter_directory_to_use_big_int.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2007 Rising Sun Pictures and Matthew Landauer
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Move the size cache from cached_sizes to directories table
+class AlterDirectoryToUseBigInt < ActiveRecord::Migration
+  def self.up
+    # allow for a zettabyte
+    change_column :directories, :count, :integer, :limit => 21,  :null => false, :default => 0
+  end
+  def self.down
+      change_column :directories, :count, :integer, :null => false, :default => 0
+
+
+  end
+end
